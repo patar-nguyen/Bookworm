949529f834e22f70c4705fc28990fc145d42275c
Patrick Nguyen <patar.nguyen@gmail.com>
2023-09-21 14:12:45 -0400

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Patrick Nguyen on 2023-09-21.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
